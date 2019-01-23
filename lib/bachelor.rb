e0f2fd4a985a765e2fdb67e55806afa1295d57d7
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 22:13:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
   count = 0
   average = 0
   data.fetch(season).each do |contestant|
-      sum = contestant.fetch("age").to_i 
+      sum += contestant.fetch("age").to_i 
       count+=1
   end
   average = sum / count
