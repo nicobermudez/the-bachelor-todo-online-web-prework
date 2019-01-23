6428ee13532bf40e7aff93cd29a30d6ef518c424
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 22:02:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,13 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
-  
+    name = ""
+  data.fetch(season).each do |contestant|
+    if contestant.has_value?(occupation)
+      name = contestant.fetch("name").split.first 
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
