bf33848419256772c51ff69c00a8ce7ef727dfee
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-23 22:01:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,15 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  name = ""
   data.fetch(season).each do |contestant|
     if contestant.has_value?("Winner")
-      name = contestant.fetch("name").split.first 
+      contestant.fetch("name").split.first 
     end
   end
-  name
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
